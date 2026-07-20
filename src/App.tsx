4151841a18b1f0d0c8fcf3588ceefb0335d75db6
A1d1yar2904 <241563@astanait.edu.kz>
2026-07-20 22:54:12 +0500

    Added category pie chart with useMemo optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import DashboardLayout from "./components/DashboardLayout";
 import SummaryCards from "./features/dashboard/SummaryCards";
+import CategoryPieChart from "./features/dashboard/CategoryPieChart";
 import { mockTransactions } from "./utils/mockData"
 
 function App(){
     return(
         <DashboardLayout>
             <SummaryCards transactions={mockTransactions} />
-            <div className="mt-8 border-4 border-dashed-4 border-slate-200 rounded-lg h-64 flex items-center justify-center">
-                <p className="text-slate-400 font-medium">
-                    Charts will be there!
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                <CategoryPieChart transactions={mockTransactions}/>
+                
+                <div className="bg-white rounded-xl p-6 border border-slate-200 shadow-sm flex items-center justify-center">
+                <p className="text-slate-400 font-serif font-medium">
+                    Recent Transactions will be there!
                 </p>
+            </div>
             </div>
         </DashboardLayout>
     );
---
+++ src/features/dashboard/CategoryPieChart.tsx
@@ -0,0 +1,63 @@
+import { useMemo } from "react"
+import { PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import type { Transaction } from "../../types/finance"
+
+interface CategoryPieChartProps {
+    transactions: Transaction[];
+}
+
+// Colors for Recharts
+const COLORS = ['#3b82f6', '#10b981', '#ef4444', '#f59e0b', '#6366f1', '#ec4899'];
+
+function CategoryPieChart({transactions}: CategoryPieChartProps){
+
+    //Filtering only Expenses
+    const chartData = useMemo(() => {
+        const expenses = transactions.filter((t) => t.type === 'expense'); 
+
+        //Groupping sums by categories
+        const totals = expenses.reduce((acc, t) => {
+            acc[t.category] = (acc[t.category] || 0) + t.amount;
+            return acc;
+        }, {} as Record<string, number>);
+
+    //Converting object ot array, which will be appropriate for Recharts
+        return Object.keys(totals).map((category) => ({
+            name: category,
+            value: totals[category], 
+        }));
+    }, [transactions]);
+
+    return(
+        <div className="bg-white rounded-xl p-6 border border-slate-200 shadow-sm h-80 flex flex-col">
+            <h3 className="font-serif text-lg font-semibold text-slate-800 mb-4">
+                Expenses by Category
+            </h3>
+            <div className="flex-1 min-h-0">
+                <ResponsiveContainer width="100%" height="100%">
+                    <PieChart>
+                        <Pie
+                            data = {chartData}
+                            cx="50%"
+                            cy="50%"
+                            innerRadius={60}
+                            outerRadius={80}
+                            paddingAngle={5}
+                            dataKey="value"
+                        >
+                            {chartData.map((_, index) => (
+                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]}/>
+                            ))}
+                        </Pie>
+                        <Tooltip formatter={(value) => `$${value}`} />
+                        <Legend />
+                    </PieChart>
+                </ResponsiveContainer>
+            </div>
+        </div> 
+    );
+
+    
+
+}
+export default CategoryPieChart
