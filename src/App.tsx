a7073defe6b3a7697ddb544f486437a520cb2f45
A1d1yar2904 <241563@astanait.edu.kz>
2026-07-22 04:43:32 +0500

    Added transaction modal with real-time state updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,60 @@
+import { useState } from "react";
 import DashboardLayout from "./components/DashboardLayout";
 import SummaryCards from "./features/dashboard/SummaryCards";
 import CategoryPieChart from "./features/dashboard/CategoryPieChart";
 import RecentTransactions from "./features/dashboard/RecentTransactions";
+import AddTransactionModal from "./features/dashboard/AddTransactionModal";
 import { mockTransactions } from "./utils/mockData"
+import type { Transaction } from "./types/finance";
+import { Plus } from "lucide-react";
 
 function App(){
+
+    // Wrapping mockTransaction to useState
+    const [transactions, setTransactions] = useState<Transaction[]>(mockTransactions);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    // Function which adds new Transaction at the start of the Array
+    const handleAddTransaction = (newTx: Transaction) => {
+        setTransactions((prev) => [newTx, ...prev]);
+    }
+
     return(
         <DashboardLayout>
-            <SummaryCards transactions={mockTransactions} />
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-                <CategoryPieChart transactions={mockTransactions}/>
-                <RecentTransactions transactions={mockTransactions} />
+            <div className="flex justify-between items-center mb-6">
+                <div>
+                    <h2 className="font-serif font-bold text-2xl text-slate-800">
+                        Financial Overview
+                    </h2>
+                    <p className="text-slate-500 text-sm">
+                        Track your income and expenses
+                    </p>
+                </div>
+            
+            {/* Switching isModalOpen to true  */}
+                <button
+                    onClick = {() => setIsModalOpen(true)}
+                    className="flex items-center gap-2 bg-blue-600 hover:bg-blue-700 hover:scale-105 text-white px-4 py-2.5 rounded-xl transition-colors font-bold text-sm shadow-sm"
+                >
+                    <Plus size={18} />
+                    Add Transaction
+                </button>
             </div>
+
+            <SummaryCards transactions={transactions} />
+
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                <CategoryPieChart transactions={transactions}/>
+                <RecentTransactions transactions={transactions} />
+            </div>
+
+            {/* Adding modal and giving props to it */}
+            <AddTransactionModal 
+                isOpen = {isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                onAddTransaction={handleAddTransaction}
+            />
+
         </DashboardLayout>
     );
 }
---
+++ src/features/dashboard/AddTransactionModal.tsx
@@ -0,0 +1,136 @@
+import { useState } from "react";
+import { X } from "lucide-react"
+
+interface AddTransactionModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    onAddTransaction: (transaction: Transaction) => void;
+};
+
+function AddTransactionModal({isOpen, onClose, onAddTransaction}: AddTransactionModalProps){
+
+    const [title, setTitle] = useState('');
+    const [amount, setAmount] = useState('');
+    const [type, setType] = useState<'income' | 'expense'>('expense');
+    const [category, setCategory] = useState('');
+
+    // if modal is closed(isOpen === false), then React will just return null (nothing will be drawn)
+    if(!isOpen) return null
+
+    // Form submit handler
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault()
+
+        if(!title || !amount || Number(amount) <= 0 || !category) return;
+
+        const newTransaction: Transaction = {
+            id: crypto.randomUUID(),
+            title,
+            amount: Number(amount),
+            type,
+            category, 
+            date: new Date().toISOString().split('T')[0],
+        };
+
+        // Sending the Transaction to App.tsx
+        onAddTransaction(newTransaction);
+
+        setTitle('');
+        setAmount('');
+        setCategory('');
+
+        // Closing the Modal
+        onClose();
+
+    };
+
+
+    return(
+        <div className="fixed inset-0 bg-slate-900/50 backdrop-blur-sm flex items-center justify-center z-50 p-4">
+            <div className="bg-white rounded-2xl w-full max-w-md p-6 shadow-xl border border-slate-100">
+                <div className="flex items-center justify-between mb-4">
+                    <h3 className="font-serif text-xl font-bold text-slate-800">
+                        New Transaction
+                    </h3>
+                    <button
+                        onClick={onClose}
+                        className="text-slate-400 hover:text-slate-600 transition-colors"
+                    >
+                        <X size={20}/>
+                    </button>
+                </div>
+
+                {/* 1. Title */}
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <div>
+                        <label className="block text-sm font-medium text-slate-700 mb-1 font-serif">
+                            Title
+                        </label>
+                        <input type="text" placeholder="e.g., Grocery Shopping" value={title} 
+                            onChange={(e) => setTitle(e.target.value)} 
+                            className="w-full px-4 py-2 border border-slate-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500/20 focus:border-blue-500 transition-colors"
+                        />
+                    </div>
+                {/* 2 и 3. Two Columns(Sum and Type) */}
+                    <div className="grid grid-cols-2 gap-4">
+                        <div>
+                            <label className="block text-sm font-medium text-slate-700 mb-1 font-serif">
+                                Amount($)
+                            </label>
+                            <input type="number" placeholder="0.00" value={amount} 
+                                onChange={(e) => setAmount(e.target.value)}
+                                className="w-full px-4 py-2 border border-slate-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500/20 focus:border-blue-500 transition-colors"
+                            />
+                        </div>
+
+                        <div>
+                            <label className="block text-sm font-medium text-slate-700 mb-1 font-serif">
+                                Type
+                            </label>
+                            <select 
+                                value={type}
+                                onChange={(e) => setType(e.target.value as 'income' | 'expense')}
+                                className="w-full px-4 py-2 border border-slate-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500/20 focus:border-blue-500 transition-colors"
+                            >
+                                <option value="expense">Expense</option>
+                                <option value="income">Income</option>
+                            </select>
+                        </div>
+                    </div>
+                
+                {/* 4. Cateories */}
+                    <div>
+                        <label className="block text-sm font-medium text-slate-700 mb-1 font-serif">
+                            Category
+                        </label>
+                        <input 
+                            type="text" 
+                            placeholder="Food, Rent, Salary..."
+                            value={category}
+                            onChange={(e) => setCategory(e.target.value)}
+                            className="w-full px-4 py-2 border border-slate-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500/20 focus:border-blue-500 transition-colors"
+                        />
+                    </div>
+
+                    <div className="flex gap-3 pt-2">
+                        <button
+                            type="button"
+                            onClick={onClose}
+                            className="flex-1 px-4 py-2 border border-slate-200 rounded-lg bg-slate-100 hover:bg-slate-200 cursor-pointer transition-colors font-medium font-serif"
+                        >
+                            Cancel
+                        </button>
+                        <button
+                            type="submit"
+                            className="flex-1 px-4 py-2 border border-slate-200 rounded-lg bg-blue-500 hover:bg-blue-600 cursor-pointer transition-colors font-medium font-serif"
+                        >
+                            Add
+                        </button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    );
+}
+export default AddTransactionModal
